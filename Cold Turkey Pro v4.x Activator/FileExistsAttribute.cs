733023818f71c51b30594a23b7f751432e5f6adf
oSumAtrIX <johan.melkonyan1@web.de>
2022-02-09 01:19:34 +0100

    Init

---
+++ Cold Turkey Pro v4.x Activator/FileExistsAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace CtActivator
+{
+	internal class FileExistsAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is string path && File.Exists(path))
+			{
+				return ValidationResult.Success;
+			}
+			return new ValidationResult($"The file '{value}' has not been found.");
+		}
+	}
+}
+
+	
+
---
+++ Cold Turkey Pro v4.x Activator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
+using Cocona;
+using CtActivator;
+
+CoconaApp.Run(([Argument(Description = "Cold Turkey Blocker.exe")][FileExists]string file) =>
+{
+	ModuleDefinition? module = null;
+	
+	try
+	{
+		module = ModuleDefinition.FromFile(file);
+	}
+	catch (BadImageFormatException)
+	{
+		Console.WriteLine("Input file does not contain a .NET module.");
+		return;
+	}
+
+	if (module?.Name?.Contains("Cold Turkey Blocker") is bool containsName && containsName)
+	{	
+		Console.WriteLine("This is not Cold Turkey Blocker.exe. Aborting.");
+		return;
+	}
+
+	TypeDefinition? additionalsType = module?.GetAllTypes()
+		.SingleOrDefault(t => t?.Name == "Additionals");
+
+	if (additionalsType is null)
+	{
+		Console.WriteLine("Unable to get the type definition which needs to be patched.");
+		return;
+	}
+
+	MethodDefinition? proStatusGetterMethod = additionalsType
+		.Methods
+		.SingleOrDefault(m => m.Name == "get_ProStatus");
+
+	if (proStatusGetterMethod is null)
+	{
+		Console.WriteLine("The method which checks for pro status could not be found.");
+		return;
+	}
+	
+	CilInstructionCollection instructions = proStatusGetterMethod.CilMethodBody?.Instructions!;
+
+	if (instructions[0].OpCode.Code == CilCode.Ldstr)
+	{
+		Console.WriteLine("File already patched. Aborting.");
+		return;
+	}
+	
+	instructions.RemoveAt(0);
+	instructions[0] = new CilInstruction(CilOpCodes.Ldstr, "pro");
+
+	try
+	{
+		File.Move(file , file + ".bak");
+	}
+	catch (Exception)
+	{
+		Console.WriteLine("The original file could not be backed up. Back it up manually and proceed by pressing the enter key.");
+		Console.ReadLine(); 
+	}
+	
+	try
+	{
+		module?.Write(file);
+	}
+	catch (AggregateException)
+	{
+		Console.WriteLine("The file was patched successfully but could not be constructed.");
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine($"Failed to save the file to disk. Try running with administrative permissions.\nDetails: {ex.Message}");
+	}
+});
